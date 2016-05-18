f28fbe5a73b5d6338e0ff11e3e371954a276b552
rollflip <thomasadam83@hotmail.com>
2016-05-18 04:39:30 +0000

    Set up database connection

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
-<?php phpinfo(); ?>
+<?php
+
+include( 'bootstrap.php' );
+
+$query = 'SELECT *
+  FROM test';
+$result = mysql_query( $query, $connect ) or die( mysql_error() );
+
+while( $record = mysql_fetch_assoc( $result ) )
+{
+  
+  echo '<pre>';
+  print_r( $record );
+  echo '</pre>';
+  
+}

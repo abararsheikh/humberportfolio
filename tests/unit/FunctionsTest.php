559151f84dcaa795251fdfb11275bc7f0c798354
Yi Zhao <zhao1max@gmail.com>
2016-05-25 15:12:36 -0400

    setup random_string test

---
+++ tests/unit/FunctionsTest.php
@@ -0,0 +1,37 @@
+<?php
+
+
+class FunctionsTest extends \Codeception\TestCase\Test
+{
+    use \Codeception\Specify;
+
+    protected function _after() {
+      // run it 10 times to have some randomness
+      for ($i = 0; $i < 30; $i++)
+      {
+        $this->testRandom_string();
+      }
+    }
+
+  // test for random_string function in functions/strings.php
+    public function testRandom_string()
+    {
+        $this->specify('default password length should be 8', function() {
+          $this->assertEquals(8, strlen(random_string()));
+        });
+
+        $this->specify('type of returned password should be string', function() {
+          $this->assertTrue(is_string(random_string()));
+        });
+
+        $this->specify('there should be no special characters in password', function() {
+          $specialChars = '/[!@#$%^&*()]/';
+          $this->assertFalse(preg_match($specialChars, random_string()) == 1);
+        });
+
+//        // this should fail the test.
+//        $this->specify('type of password should not be number', function() {
+//          $this->assertTrue(is_numeric(random_string()));
+//        });
+    }
+}

6d37108dc3a088107524553583564a9e205544d4
Adam <adamthomashumber@gmail.com>
2016-05-25 16:12:42 +0000

    Added fore files

--- bootstrap.php
+++ bootstrap.php
@@ -1,6 +1,10 @@
 <?php
 
+session_start();
+
 include( 'config.php' );
+
+include( 'functions/strings.php' );
 
 $connect = mysql_connect( MYSQL_HOST, MYSQL_USERNAME, MYSQL_PASSWORD ) or die( mysql_error() );
 mysql_select_db( MYSQL_DATABASE, $connect ) or die( mysql_error() );

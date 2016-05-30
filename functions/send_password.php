904ed4901e0aee5a22233dc97aecfe6e8a1488a4
JonBoss5 <JonBoss1@hotmail.com>
2016-05-30 17:19:33 +0000

    send_password created

---
+++ functions/send_password.php
@@ -0,0 +1,10 @@
+<?php
+
+//This function accepts a user's email as a parameter, and calls the mailer function to retireve the user's password
+
+//Are we using the depricated my_sql to connect? can we instead use PDO?
+
+function send_password ($email_parameter){
+  //What table should I be retrieving the password from?
+  //Is this for administratos or students?
+}

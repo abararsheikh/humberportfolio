3c94b20138bb6f21126e319a82fa79bb1fd5047d
Jordan Carter <jordan.trombone@gmail.com>
2016-06-01 15:23:27 +0000

    first phase of HTML and PHP for change_password form, and made frontend folder for the group

---
+++ frontend/change_password.php
@@ -0,0 +1,14 @@
+<!-- Change password form -->
+<form action="change_password_handler.php" method="POST">
+  <!-- Password -->
+  <label for="password">Password:</label>
+  <input type="password" name="password" id="password" />
+  <!-- error message goes here -->
+  <span class="err-msg"><?php if(isset($_POST['submit'])) { echo $err } ?></span>
+  <!-- Confirm password -->
+  <label for="password_confirm">Password:</label>
+  <input type="password" name="password_confirm" id="password_confirm" />
+  <!-- error message goes here -->
+  <span class="err-msg"><?php if(isset($_POST['submit'])) { echo $err } ?></span>
+  <input type="submit" value="submit" name="submit" />
+</form>

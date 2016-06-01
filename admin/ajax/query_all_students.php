6293aca3b004d35bbeea022acd4d2c58baca398e
Gord Schnurr <gordschnurr@gmail.com>
2016-06-01 15:06:17 +0000

    added Query_all_students.php

--- admin/ajax/query_all_students.php
+++ admin/ajax/query_all_students.php
@@ -1,7 +1,14 @@
 <?php
 require_once '../../bootstrap.php'; // database file
 // TODO: once function is ready, add call to check whether current user is logged in as admin role
-// 
+// TODO: add check of post data
 
 $db = Database::getDB();
-var_dump($db);
+$query = 'select * from students';
+$prepared = $db->prepare($query);
+$prepared->execute();
+$prepared->setFetchMode(PDO::FETCH_OBJ);
+$results = $prepared->fetchAll();
+
+header('Content-Type: application/json');
+echo json_encode($results);

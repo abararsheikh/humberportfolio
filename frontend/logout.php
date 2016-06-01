03d36bf9f653c40044fc31df0cd3f88c923e857e
abararsheikh <er.abrar@gmail.com>
2016-06-01 17:49:55 +0000

    logout file added

--- frontend/logout.php
+++ frontend/logout.php
@@ -1,4 +1,4 @@
 <?php
 include( 'bootstrap.php' );
 session_destroy();  
-header("Location: ./index.php");//use for the redirection to Home page
+header("Location: ../index.php");//use for the redirection to Home page

dd79b4878764349c4c22bc61c9155c2496cb31b3
JonBoss5 <JonBoss1@hotmail.com>
2016-06-01 15:24:48 +0000

    password functions updated, require db information

--- bootstrap.php
+++ bootstrap.php
@@ -5,6 +5,8 @@ session_start();
 include( 'config.php' );
 
 include( 'functions/strings.php' );
+include( 'forgot_password.php' );
+include( 'change_password.php' );
 
 //$connect = mysql_connect( MYSQL_HOST, MYSQL_USERNAME, MYSQL_PASSWORD ) or die( mysql_error() );
 //mysql_select_db( MYSQL_DATABASE, $connect ) or die( mysql_error() );
---
+++ functions/forgot_password.php
@@ -0,0 +1,34 @@
+<?php
+
+//This function accepts a user's email as a parameter, and calls the mailer function to retireve the user's password
+//After calling the mailer function, send_password will return a user message that can be implemented on a results page.
+
+function forgot_password ($email_parameter, $type_parameter){
+  //generate new random character string for password
+  $new_password = random_string();
+  
+  //replace user's password in database
+  $client_message = change_password($email_parameter, $new_password, $type_parameter);
+  $client_message .= ' - please check your email for instructions.';
+  
+  //email new password to user - call Zarna's function
+  $email_subject = 'Password Reset';
+  $email_message = "
+  <html>
+  <head>
+  <meta name="viewport" content="width=device-width">
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <title>Password Reset</title>
+  <body>
+    <h1>Your Password has been reset</h1>
+    <p>Please follow the link below to access your account by typing in the new password provided</p>
+    <h2>$new_password</h2>
+    <a href='#'>Login to your account</a>
+  </body>
+  </html>";
+  
+  send_email($email_parameter,$email_subject,$email_message);
+  
+  //return message to front end for results page
+  return $client_message;
+}

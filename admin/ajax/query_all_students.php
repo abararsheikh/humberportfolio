428ddfc10158f267a6502ad3ced059594e76ff42
Gord Schnurr <gordschnurr@gmail.com>
2016-06-01 15:01:52 +0000

    added basic ajax call script for querying all students

---
+++ admin/ajax/query_all_students.php
@@ -0,0 +1,7 @@
+<?php
+require_once '../../bootstrap.php'; // database file
+// TODO: once function is ready, add call to check whether current user is logged in as admin role
+// 
+
+$db = Database::getDB();
+var_dump($db);

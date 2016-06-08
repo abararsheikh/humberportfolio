83434e749f2aa78025f83cb56301f4f948e635d4
Jordan Carter <jordan.trombone@gmail.com>
2016-06-08 16:33:31 +0000

    added server validation, and modified structure

--- frontend/change_password.php
+++ frontend/change_password.php
@@ -1,17 +1,35 @@
 <!-- Change password form -->
 <form action="change_password_handler.php" method="POST" id="change_pass_form">
+  
   <!-- Password -->
-  <label for="password">New Password:</label>
-  <input type="password" name="password" id="password" />
-  <!-- error message goes here -->
-  <span class="err-msg"><?php if(isset($_POST['submit'])) { echo $err; } ?></span>
+  <div class="jc-field-wrapper">  
+    <label for="password">New Password:</label>
+    <input type="password" name="password" id="password" />
+    
+    <!-- error message -->
+    <div class="jc-pass-err-wrap">
+      <?php if(isset($_COOKIE['emptyErr'])) { echo $_COOKIE["emptyErr"]; } ?>
+      <?php if(isset($_COOKIE['matchErr'])) { echo $_COOKIE["matchErr"]; } ?>
+    </div><!-- /error message -->
+    
+  </div><!-- /Password -->
+  
   <!-- Confirm password -->
-  <label for="password_confirm">Confirm Password:</label>
-  <input type="password" name="password_confirm" id="password_confirm" />
-  <!-- error message goes here -->
-  <span class="err-msg"><?php if(isset($_POST['submit'])) { echo $err; } ?></span>
+  <div class="jc-field-wrapper">  
+    <label for="password_confirm">Confirm Password:</label>
+    <input type="password" name="password_confirm" id="password_confirm" />
+    
+    <!-- error message -->
+    <div class="jc-pass-err-wrap">
+      <?php if(isset($_COOKIE['emptyErr'])) { echo $_COOKIE["emptyErr"]; } ?>
+      <?php if(isset($_COOKIE['matchErr'])) { echo $_COOKIE["matchErr"]; } ?>
+    </div><!-- /error message -->
+    
+  </div><!-- /Confirm password -->
+  
   <input type="submit" value="submit" name="btn-submit" id="pass-submit" />
 </form>
 
 <script src="../vendor/components/jquery/jquery.min.js"></script>
 <script src="scripts/change_password.js"></script>
+
--- frontend/scripts/change_password.js
+++ frontend/scripts/change_password.js
@@ -1,49 +1,58 @@
-$(document).ready(function () {
+$(document).ready(function () 
+{
   
   //Inline validation
-  $('#password_confirm').change( function () {
+  $('#password_confirm').change( function () 
+  {
     checkPasswords();
   });
   
   //Check for validation before submitting
-  $('#pass-submit').click( function(e) {
+  $('#pass-submit').click( function(e) 
+  {
     e.preventDefault();
     var passed = checkPasswords();
     var passedValidation = validate();
     
-    if (passed && passedValidation) {
+    if (passed && passedValidation)
+    {
       $('#change_pass_form').submit();
     }
-    
   });
   
 });
 
-function checkPasswords() {
+function checkPasswords()
+{
   
   var passed;
   
-  if ( $('#password_confirm').val() != $('#password').val() ) {
-      $('.err-msg').html("Passwords must match!");
-      passed = false;
-    }
-    else{
-      $('.err-msg').html("");
-      passed = true;
-    }
+  if ( $('#password_confirm').val() != $('#password').val() ) 
+  {
+    $('.jc-pass-err-wrap').html("<span class='jc-match-err jc-pass-err''>Passwords must match!</span>");
+    passed = false;
+  }
+  else
+  {
+    $('.jc-pass-err-wrap').html("");
+    passed = true;
+  }
   
   return passed;
   
 }
 
-function validate() {
+function validate() 
+{
   
   var passed = "";
   
-  $('#change_pass_form input').each( function () {
+  $('#change_pass_form input').each( function () 
+  {
     // Validate empty fields
-    if ( $(this).val() === "" ) {
-      $(this).next('.err-msg').html("*Required");
+    if ( $(this).val() === "" )
+    {
+      $(this).next('.jc-pass-err-wrap').html("<span class='jc-req-err jc-pass-err''>*Required.</span>");
       passed += false;
     }
   });

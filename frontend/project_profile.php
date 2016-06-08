d4a0792aa098d00090b66830051208c2c871a4e7
abararsheikh <er.abrar@gmail.com>
2016-06-08 18:45:01 +0000

    Project profile page is added

--- frontend/project_profile.php
+++ frontend/project_profile.php
@@ -10,8 +10,8 @@
       $statement->closeCursor();
 ?>
 <div>
-  <table>
-    <thead>
+  <table class="table table-bordered" border="1">
+    <thead style="color:rosybrown;font-size: 24px;">
       <tr>
         <th>Name</th>
         <th>Description</th>
@@ -19,11 +19,12 @@
         <th>Link</th>
       </tr>
     </thead>
-    
+      
     <?php foreach($result as $projects) : ?>
     <tbody>
       <tr>
-        <td></td>
+        <td><?php echo $projects['name']; ?></td>
+         <td><?php echo $projects['description']; ?></td>
       </tr>
     </tbody>
     <?php endforeach; ?>

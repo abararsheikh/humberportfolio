807618a42508ca018f96f715bdc2e448b69149aa
Yi Zhao <zhao1max@gmail.com>
2016-06-15 14:52:56 -0400

    add change password test

--- tests/acceptance/LoginCept.php
+++ tests/acceptance/LoginCept.php
@@ -3,8 +3,8 @@ $I = new AcceptanceTester($scenario);
 $I->am('student');
 $I->wantTo('Login');
 $I->amOnPage('/');
+$I->canSee('Welcome to Portfolio Tool');
 $I->fillField('email', 'adam.potter@gmail.com');
 $I->fillField('password', '12345');
 $I->click('Login');
 $I->see('Welcome : Adam');
-

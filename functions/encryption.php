c95b3a0157dffcfef4dc1c8f5c35ecd7c538c269
Reza <sakhapour2@gmail.com>
2016-06-22 16:06:58 +0000

    add comments to encrytion.php

--- functions/encryption.php
+++ functions/encryption.php
@@ -1,4 +1,10 @@
 <?php
+
+/**
+ * This function is encrypt the password. 
+ * @param string $new_password new password
+ * @return string $new_password new encrypted password
+*/
 function hash($password)
 {
     $new_password = password_hash($password, PASSWORD_DEFAULT);

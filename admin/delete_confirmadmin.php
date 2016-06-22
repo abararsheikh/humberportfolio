549156f13d814609fab558739eab7ef4765d6e91
LoveleenAnand <loveleen.anand@outlook.com>
2016-06-22 13:25:08 +0000

    Codee added to add time

--- admin/delete_confirmadmin.php
+++ admin/delete_confirmadmin.php
@@ -3,7 +3,7 @@ include( '../bootstrap.php' );
 
   $db = Database::getDB();
   $studentId = $_POST['student_id'];
-  $today = date('Y-m-d') ;
+  $today = date('Y-m-d H:i:s') ;
   $query = "UPDATE students SET deleted_at = '$today' WHERE id = '$studentId' ";
 //$query = "DELETE FROM students WHERE id = '$studentId' ";
     
--- admin/updatestudent.php
+++ admin/updatestudent.php
@@ -15,7 +15,7 @@ $Bio = $_POST['Bio'];
 $social_media = $_POST['social_media'];
 // Image
 $fp=addslashes(file_get_contents($_FILES['uploadedimage']['tmp_name'])); //will store the image to fp
- $today = date('Y-m-d') ;
+ $today = date('Y-m-d H:i:s') ;
 
 //Documents folder, should exist in your host in there you're going to save the file just uploaded
 

7ad6c43d63ace7caec689c0e855f559160a487af
akshi <akshi.gakhar@gmail.com>
2016-06-22 18:14:08 +0000

    Resizing the image using wide image added

--- functions/resizeImage.php
+++ functions/resizeImage.php
@@ -1,6 +1,8 @@
 <?php 
 include 'WideImage/WideImage.php';
-function resizeImage($image, $width, $height, $quality)
+//function resizes according to the width and height as specified
+//it also changes the quality of the image
+function doresize($image, $width, $height, $quality)
 {
   $originalImage = WideImage::load($image);
   $newImage = $image->resize($width, $height);
--- functions/resize_image.php
+++ functions/resize_image.php
@@ -1,6 +1,6 @@
 <?php
 
-function resizeimageimage ($tablename, $id) {
+function resizeimageimage ($tablename, $id, $width, $height, $quality) {
       try {
           // connect to database and fetch image
           $db = Database::getDB();
@@ -9,7 +9,7 @@ function resizeimageimage ($tablename, $id) {
           $image = $db->prepare($query);
           $image->execute();
           // call the doresize function to resize the image. This function will return the image  
-          return doresize($image);
+          return doresize($image, $width, $height, $quality);
       }
       //catch and return errors
       catch (PDOException $e){

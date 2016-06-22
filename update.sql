28f38a0de36c668596f0d17e16ff7ebed09ea12e
Lee <leejsitu@gmail.com>
2016-06-22 15:17:53 +0000

    Added index sql statements

--- update.sql
+++ update.sql
@@ -21,13 +21,22 @@ ALTER TABLE `students` CHANGE `deleted_at` `deleted_at` DATETIME NULL;
 ALTER TABLE `students` CHANGE `updated_at` `updated_at` DATETIME NULL;
 
 
-
 --INDICES
 --project name
 --image alt text
 --project link
 --email
 --project keywords as FULL TEXT
+
+ALTER TABLE `projects` ADD INDEX(`name`);
+
+ALTER TABLE `images` ADD FULLTEXT(`alt`);
+
+ALTER TABLE `students` ADD FULLTEXT(`website_link`);
+
+ALTER TABLE `students` ADD INDEX(`email`);
+
+ALTER TABLE `projects` ADD FULLTEXT(`keywords`);
 
 --CHECK CURRENT VERSION OF THE DB
 --SELECT MAX(UPDATE_TIME), MAX(CREATE_TIME), TABLE_SCHEMA FROM `TABLES` GROUP BY TABLE_SCHEMA ORDER BY 1, 2

f1c15bd3712c8b736b52fa82b9d7a4476df376c2
Jordan Carter <jordan.trombone@gmail.com>
2016-06-22 16:27:51 +0000

    commented lines in index.php

--- index.php
+++ index.php
@@ -3,8 +3,7 @@
 include( 'bootstrap.php' );
 include "./frontend/login.php";
 
-<<<<<<< HEAD
-$query = 'SELECT *
+/*$query = 'SELECT *
   FROM test';
 $result = mysql_query( $query, $connect ) or die( mysql_error() );
 
@@ -16,9 +15,8 @@ while( $record = mysql_fetch_assoc( $result ) )
   //echo $record['name'];
   echo '</pre>';
   
-}
+}*/
 
-=======
 ?>
 
 <html>
@@ -65,4 +63,3 @@ while( $record = mysql_fetch_assoc( $result ) )
     
   </body>
 </html>
->>>>>>> 496064d23acac08a5ae8beaf1195cff7b915d7d5

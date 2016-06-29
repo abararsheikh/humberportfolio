b7aac0a4b381855f012b16ef9f60f4562400ad7b
Alya Talat <tltl0006@humbermail.ca>
2016-06-29 05:37:24 +0300

    Update resize_image.php
    used bind param

--- functions/resize_image.php
+++ functions/resize_image.php
@@ -1,12 +1,14 @@
 <?php
 
-function resizeimageimage ($tablename, $id, $width, $height, $quality) {
+function resizeimage ($tablename, $id, $width, $height, $quality) {
       try {
           // connect to database and fetch image
           $db = Database::getDB();
           $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-          $query = "SELECT image from $tablename WHERE id=$id";
+    
+          $query = "SELECT image from $tablename WHERE id=$id AGAINST (:query);";
           $image = $db->prepare($query);
+          $image->bindParam(':query', $query);
           $image->execute();
           // call the doresize function to resize the image. This function will return the image  
           return doresize($image, $width, $height, $quality);
